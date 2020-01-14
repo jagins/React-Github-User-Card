18a6f70cfc1d0eb0b70ec8bbe2b770744c0326cb
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-01-14 14:16:09 -0600

    Added in details for the card will pass in props later

--- react_github_user_card/src/Componets/Card.js
+++ react_github_user_card/src/Componets/Card.js
@@ -1,10 +1,16 @@
 import React from 'react';
 
-function Card()
+function Card(props)
 {
     return(
-        <div>
-            
+        <div className='card'>
+            <div className='card-info'>
+                <h3>name</h3>
+                <p className='username'>username</p>
+                <p>Profile: <a href='address here'>address here too</a></p>
+                <p>Followers: followers</p>
+                <p>Following: follow count</p>
+            </div>
         </div>
     );
 }
