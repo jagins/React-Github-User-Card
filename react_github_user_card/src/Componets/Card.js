e5e8a589a2452a9e9b4db2620ae30d2327f38081
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-01-14 14:44:53 -0600

    Fixed up card

--- react_github_user_card/src/Componets/Card.js
+++ react_github_user_card/src/Componets/Card.js
@@ -4,9 +4,11 @@ function Card(props) {
     const { img, username, name, location, profile, followersCount, followingCount } = props;
     return (
         <div className='card'>
+            <img src={img}/>
             <div className='card-info'>
                 <h3>{name}</h3>
                 <p className='username'>{username}</p>
+                <p>{location}</p>
                 <p>Profile: <a href={profile} target='_blank'>{profile}</a></p>
                 <p>Followers: {followersCount}</p>
                 <p>Following: {followingCount}}</p>
