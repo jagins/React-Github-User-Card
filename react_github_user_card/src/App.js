5f0eda5755bbf53b846c26e32228a52e01855e4e
Joshua Agins <56888002+jagins@users.noreply.github.com>
2020-01-14 18:14:19 -0600

    Added in followers and stylying MVP Complete!

--- react_github_user_card/src/App.js
+++ react_github_user_card/src/App.js
@@ -1,10 +1,9 @@
-import React, {Component} from 'react';
+import React, { Component } from 'react';
 import axios from 'axios';
 import Card from './Componets/Card';
 import './App.css';
 
-class App extends Component
-{
+class App extends Component {
   state = {
     img: '',
     username: '',
@@ -13,42 +12,79 @@ class App extends Component
     profile: '',
     followersCount: 0,
     followingCount: 0,
-    bio: ''
+    bio: '',
+    cardArray: [],
+    followerArray: []
   }
-  componentDidMount()
-  {
+
+  componentDidMount() {
     axios.get('https://api.github.com/users/jagins')
-    .then(response =>{
-      this.setState({
-        img: response.data.avatar_url,
-        username: response.data.login,
-        name: response.data.name,
-        location: response.data.location,
-        profile: response.data.html_url,
-        followersCount: response.data.followers,
-        followingCount: response.data.following
-      });
-    })
-    .catch(err => console.log('Something went wrong: ', err));
+      .then(response => {
+        this.setState({
+          cardArray: [{
+            img: response.data.avatar_url,
+            username: response.data.login,
+            name: response.data.name,
+            location: response.data.location,
+            profile: response.data.html_url,
+            followersCount: response.data.followers,
+            followingCount: response.data.following,
+            bio: response.data.bio
+          }]
+        });
+      })
+      .catch(err => console.log('Something went wrong: ', err));
+
+    this.createFollowerCard();
   }
 
-  fetch = () =>
-  {
+  createFollowerCard = () => {
+    let followerLinks = [];
+    axios.get('https://api.github.com/users/jagins/followers')
+      .then(response => {
+        this.setState({ followerArray: response.data });
+        followerLinks = this.state.followerArray.map(person => {
+          return person.url;
+        })
+        followerLinks.forEach(el => {
+          axios.get(el)
+            .then(response => {
+              this.setState({
+                cardArray: [...this.state.cardArray, {
+                  img: response.data.avatar_url,
+                  username: response.data.login,
+                  name: response.data.name,
+                  location: response.data.location,
+                  profile: response.data.html_url,
+                  followersCount: response.data.followers,
+                  followingCount: response.data.following,
+                  bio: response.data.bio
+                }]
+              })
+            })
+            .catch(err => console.log('Something went wrong: ', err));
+        })
+      })
+      .catch(err => console.log('Something went wrong: ', err));
 
   }
-
-  render()
-  {
+  render() {
     return (
       <div className="App">
-        <Card 
-          img={this.state.img}
-          username={this.state.username}
-          name={this.state.name}
-          location={this.state.location}
-          profile={this.state.profile}
-          followersCount={this.state.followersCount}
-          followingCount={this.state.followingCount}/>
+        <div className='container'>
+          {this.state.cardArray.map(card => {
+            return <Card
+              key={card.username}
+              img={card.img}
+              username={card.username}
+              name={card.name}
+              profile={card.profile}
+              followersCount={card.followersCount}
+              followingCount={card.followersCount}
+              bio={card.bio}
+            />
+          })}
+        </div>
       </div>
     );
   }
--- react_github_user_card/src/Componets/Card.js
+++ react_github_user_card/src/Componets/Card.js
@@ -1,10 +1,10 @@
 import React from 'react';
 
 function Card(props) {
-    const { img, username, name, location, profile, followersCount, followingCount } = props;
+    const { img, username, name, location, profile, followersCount, followingCount, bio } = props;
     return (
         <div className='card'>
-            <img src={img}/>
+            <img src={img} />
             <div className='card-info'>
                 <h3>{name}</h3>
                 <p className='username'>{username}</p>
@@ -12,6 +12,7 @@ function Card(props) {
                 <p>Profile: <a href={profile} target='_blank'>{profile}</a></p>
                 <p>Followers: {followersCount}</p>
                 <p>Following: {followingCount}</p>
+                <p>Bio: {bio}</p>
             </div>
         </div>
     );
